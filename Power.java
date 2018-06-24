0b8c2da66f0be1862cec36944133d886f1a97eea
S MANOJ KUMAR <36785034+manojscs010@users.noreply.github.com>
2018-06-24 15:33:33 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+class Power
+{
+    public static void main(String s[])
+    {
+        int a=2,b=3;
+         System.out.print((int)Math.pow(a,b));
+    }
+}
+
