b914f5deebee787ef8c160db49cc7136ebd15396
S MANOJ KUMAR <36785034+manojscs010@users.noreply.github.com>
2018-08-29 20:12:52 +0530

    Add files via upload

---
+++ Gupow.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Gupow
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+         int c=(int)Math.pow(a,b);
+        System.out.print(c); 
+    }
+}
