a9d365511d4928ae4256cd595ad0c536263ca04a
S MANOJ KUMAR <36785034+manojscs010@users.noreply.github.com>
2018-08-29 20:35:04 +0530

    Add files via upload

---
+++ Gusort.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Gusort
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+         int a[]=new int[n];
+         int i=0;
+          for(i=0;i<n;i++)
+          {
+              a[i]=sc.nextInt();
+          }
+          Arrays.sort(a);
+          for(i=0;i<n;i++)
+         System.out.print(a[i]+" "); 
+          
+    }
+}
